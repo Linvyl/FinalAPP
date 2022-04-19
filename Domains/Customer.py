69fa13997180cb9f19541ec08c2f65616b8f1769
AnDDoanf <77445122+AnDDoanf@users.noreply.github.com>
2022-04-19 12:37:09 +0700

    modify Customer description

--- Domains/Customer.py
+++ Domains/Customer.py
@@ -3,6 +3,7 @@ property            type            range
 
 cusID               String          length = 10
 cusName             String          length <= 30
+cusPhoneNum         String          length = 10 or 9
 cusEmail            String          
 cusDoB              datetime        dd(1-31)/mm(1-12)/yyyy(1920-now)
 cusGender           String          male/female
