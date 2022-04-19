d00d2813c7362ddfd6f06a91fa0081ce35fc2ea2
AnDDoanf <77445122+AnDDoanf@users.noreply.github.com>
2022-04-19 11:12:07 +0700

    first commit:

---
+++ Domains/Customer.py
@@ -0,0 +1,9 @@
+"""
+property            type            range
+
+cusID               String          length = 10
+cusName             String          length <= 30
+cusEmail            String          
+cusDoB              datetime        dd(1-31)/mm(1-12)/yyyy(1920-now)
+cusGender           String          male/female
+"""
